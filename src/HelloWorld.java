f70d20483ff97df3342b69f4d1b0ba43f7bb88c8
ArhanH_____ <arhanh@nycstudents.net_____>
2022-09-21 20:28:33 -0400

    Added another println statement

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.print("I like turtles!");
+        System.out.println("Welcome to my world! Please enter to your right!");
 
 
 
